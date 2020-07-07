c4060213e43a557ab36afd202d010202c88d2b95
Arron Hunt <arronhunt@users.noreply.github.com>
2020-07-07 12:02:33 -0700

    Docs formatting (#37)
    * Update to Sphinx theme
    
    * added new theme to docs reqs
    
    * Basic themeing
    
    * Added logo
    
    * removed blurple
    
    * styled for mobile
    
    * Cleanup
    
    * Fixed overflow on mobile
    
    * Load custom CSS from hosed docs
    
    Co-authored-by: John Myers <john@gretel.ai>

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,14 @@ import sys
 
 import sphinx
 
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Gretel Synthetics'
-copyright = '2020, Gretel.ai'
-author = 'Gretel.ai'
+project = "Gretel Synthetics"
+copyright = "2020, Gretel.ai"
+author = "Gretel.ai"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,17 +30,23 @@ author = 'Gretel.ai'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'm2r']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "m2r",
+    "sphinx_rtd_theme",
+]
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,30 +54,35 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_logo = "img/gretel_logo_white.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["styles.css"]
 
 html_theme_options = {
-    'sidebar_width': '250px',
-    'font_family': 'Inter'
+   'logo_only': True,
+    'display_version': True,
+    'style_nav_header_background': '#0c0c0d',
 }
-
-html_css_files = [
-    'styles.css'
-]
-
 
 def monkeypatch(cls):
     """ decorator to monkey-patch methods """
+
     def decorator(f):
         method = f.__name__
         old_method = getattr(cls, method)
-        setattr(cls, method, lambda self, *args, **kwargs: f(old_method, self, *args, **kwargs))
+        setattr(
+            cls,
+            method,
+            lambda self, *args, **kwargs: f(old_method, self, *args, **kwargs),
+        )
+
     return decorator
+
 
 # workaround until https://github.com/miyakogi/m2r/pull/55 is merged
 @monkeypatch(sphinx.registry.SphinxComponentRegistry)
