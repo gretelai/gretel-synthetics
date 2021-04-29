15b2584b44f97bd7b91e04c20f862632e1f709d6
Alexander Watson <zredlined@users.noreply.github.com>
2021-04-29 08:39:54 -0700

    validation test cases and config test updates

--- tests/tensorflow/test_tf_train.py
+++ tests/tensorflow/test_tf_train.py
@@ -7,8 +7,10 @@ import pandas as pd
 from gretel_synthetics.tensorflow.train import (
     _ModelHistory,
     _save_history_csv,
-    VAL_LOSS,
-    VAL_ACC,
+    METRIC_LOSS,
+    METRIC_ACCURACY,
+    METRIC_VAL_LOSS,
+    METRIC_VAL_ACCURACY,
 )
 from gretel_synthetics.train import train
 
@@ -34,10 +36,12 @@ def test_train_rnn(save_history, model, tf_config):
 def history():
     hist = _ModelHistory(None, None)
     values = [1, 2, 3, 4, 5, 6]
-    hist.losses = deepcopy(values)
+    hist.loss = deepcopy(values)
     hist.accuracy = deepcopy(values)
-    hist.epsilons = deepcopy(values)
-    hist.deltas = deepcopy(values)
+    hist.val_loss = deepcopy(values)
+    hist.val_accuracy = deepcopy(values)
+    hist.epsilon = deepcopy(values)
+    hist.delta = deepcopy(values)
     hist.best = [0] * 6
     return hist
 
@@ -50,21 +54,21 @@ def test_save_history_no_best(tmpdir, history):
 
 
 def test_save_history_best_loss(tmpdir, history):
-    _save_history_csv(history, tmpdir, True, VAL_LOSS, 3)
+    _save_history_csv(history, tmpdir, True, METRIC_VAL_LOSS, 3)
     df = pd.read_csv(tmpdir + "/model_history.csv")
     check = df[df["best"] == 1]
     assert list(check["epoch"])[0] == 2
 
 
-def test_save_history_best_acc(tmpdir, history):
-    _save_history_csv(history, tmpdir, True, VAL_ACC, 1)
+def test_save_history_best_accuracy(tmpdir, history):
+    _save_history_csv(history, tmpdir, True, METRIC_VAL_ACCURACY, 1)
     df = pd.read_csv(tmpdir + "/model_history.csv")
     check = df[df["best"] == 1]
     assert list(check["epoch"])[0] == 0
 
 
 def test_save_history_best_not_valid(tmpdir, history):
-    _save_history_csv(history, tmpdir, True, VAL_ACC, 11)
+    _save_history_csv(history, tmpdir, True, METRIC_VAL_ACCURACY, 11)
     df = pd.read_csv(tmpdir + "/model_history.csv")
     check = df[df["best"] == 1]
     assert list(check["epoch"])[0] == 5
