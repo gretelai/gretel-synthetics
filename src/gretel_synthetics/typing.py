6d09f7ea602535c1b415f081ca9fb1882bb0943b
Andrew Carr <andrewnc@users.noreply.github.com>
2022-09-23 10:58:02 -0600

    Add ACTGAN model into the MIF
    * Initial ACTGAN model
    
    * Update transformations and tests to latest version of rdt, ctgan and sdv
    
    * Started some typing for ACTGAN, call back plumbing, better logging
    
    Co-authored-by: John Myers <9696606+johntmyers@users.noreply.github.com>
    GitOrigin-RevId: cb39906b9119adde2631eadf64a4353bf8d26bb5

---
+++ src/gretel_synthetics/typing.py
@@ -0,0 +1,13 @@
+"""
+Unified types for Gretel Synthetics
+"""
+from __future__ import annotations
+
+from typing import Any, List, Union
+
+import numpy as np
+import pandas as pd
+
+DFLike = Union[pd.DataFrame, np.ndarray, List[List[Any]]]
+SeriesOrDFLike = Union[pd.Series, DFLike]
+ListOrSeriesOrDF = Union[List[Any], List[List[Any]], SeriesOrDFLike]
